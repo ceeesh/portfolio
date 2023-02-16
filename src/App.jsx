3fb7cb8fe227743b60f0d0b050785f65c469f539
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-02-16 19:18:31 +0800

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Header from "./components/layouts/Header"
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      HELLO hey
+    <div className="bg-gray-600 w-full h-screen">
+      <Header/>
     </div>
   )
 }
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,22 @@
+import tempLogo from '../../assets/images/night-mode.png'
+import menu from '../../assets/images/menu.png'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="flex justify-between py-8 px-5 text-white">
+                <div>
+                    <a className="flex gap-3" href='#'>
+                        <img className='w-7' src={tempLogo} />
+                        <h2 className="font-bold text-lg tracking-wider">Cee</h2>
+                    </a>
+                </div>
+                <button className=''>
+                    <img className="w-7" src={menu}/>
+                </button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
