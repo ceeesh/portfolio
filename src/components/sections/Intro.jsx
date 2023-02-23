149ca603d03ed27253d8b8bb59a50c62b2f7f187
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-02-23 15:36:07 +0800

    fade animation

--- src/components/sections/Intro.jsx
+++ src/components/sections/Intro.jsx
@@ -1,18 +1,16 @@
 import { motion, useScroll } from "framer-motion"
+import { staggerContainer,fade, fadeIn } from "../../utils/framerMotion"
 
 const Intro = () => {
   return (
-    <motion.section
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
+    <motion.section variants={staggerContainer(0.4, 1.3)} initial="hidden" animate="show"
       className="relative w-full flex flex-col items-start min-h-screen justify-center mb-20">
-      <p className="text-lg mb-4 text-secondaryTxt z-20 ">Hey there! I am</p>
-      <h1 className="text-4xl font-bold mb-4 lg:text-7xl text-mainTxt z-20 ">Cee Jay Malacas</h1>
-      <h2 className="text-3xl lg:text-7xl font-bold mb-4 z-20 ">Coding for more money?!</h2>
-      <p className="text-base font-thin mb-4  z-20 ">A highly passionate Front-end Web Developer and Mobile Developer.</p>
-      <a className=" z-20 px-12 py-2.5 text-lg rounded-md border-[0.1rem] bg-btnbg border-mainTxt text-mainTxt hover:bg-btnHvr hover:-translate-y-1 transition all duration-300 delay-75">Projects</a>
-      <p className="absolute -right-2 md:-bottom-2 lg:-bottom-2 lg:right-0 text-8xl  font-bold text-btnbg flex flex-col justify-end items-end uppercase z-0">
+      <motion.p variants={fade(0.8, .3)} className="text-lg mb-4 text-secondaryTxt z-20 ">Hey there! I am</motion.p>
+      <motion.h1 variants={fade(0.8, .4)} className="text-4xl font-bold mb-4 lg:text-7xl text-mainTxt z-20 ">Cee Jay Malacas</motion.h1>
+      <motion.h2 variants={fade(0.8, .5)} className="text-3xl lg:text-7xl font-bold mb-4 z-20 ">Coding for more money?!</motion.h2>
+      <motion.p variants={fade(0.8, .7)} className="text-base font-thin mb-4  z-20 ">A highly passionate Front-end Web Developer and Mobile Developer.</motion.p>
+      <motion.a variants={fade(0.8, .7)} className=" z-20 px-12 py-2.5 text-lg rounded-md border-[0.1rem] bg-btnbg border-mainTxt text-mainTxt hover:bg-btnHvr hover:-translate-y-1 transition all duration-300 delay-75">Projects</motion.a>
+      <motion.p variants={fade(0.8, .8)} className="absolute -right-2 md:-bottom-2 lg:-bottom-2 lg:right-0 text-8xl font-bold text-btnbg flex flex-col justify-end items-end uppercase z-0">
         <span>m</span>
         <span>a</span>
         <span>l</span>
@@ -20,7 +18,7 @@ const Intro = () => {
         <span>c</span>
         <span>a</span>
         <span>s</span>
-      </p>
+      </motion.p>
     </motion.section>
   )
 }
---
+++ src/utils/framerMotion.js
@@ -0,0 +1,40 @@
+export const staggerContainer = (staggerChildren, delayChildren) => ({
+	hidden: {},
+	show: {
+		transition: {
+			staggerChildren,
+			delayChildren,
+		},
+	},
+});
+
+export const fadeIn = (direction, type, delay, duration) => ({
+	hidden: {
+		x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+		y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+		opacity: 0,
+	},
+	show: {
+		x: 0,
+		y: 0,
+		opacity: 1,
+		transition: {
+			type,
+			delay,
+			duration,
+			ease: 'easeIn',
+		},
+	},
+});
+
+export const fade = (duration, delay) => ({
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			duration: duration,
+			delay: delay,
+			ease: 'easeInOut',
+		},
+	},
+});
